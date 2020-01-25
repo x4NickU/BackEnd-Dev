eda841902b1cc8264ceb8a829ed76a0906cbec04
x4NickU <ddjnick03@gmail.com>
2020-01-25 09:00:17 +0100

    sad

--- Node.JS/NodeJS - Login APP/db_1.sql
+++ Node.JS/NodeJS - Login APP/db_1.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : myudb
+Source Server         : Mia
 Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : db_1
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2020-01-23 22:14:09
+Date: 2020-01-25 09:00:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,34 +20,37 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `composition`;
 CREATE TABLE `composition` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT '',
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
   `set` text DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of composition
 -- ----------------------------
+INSERT INTO `composition` VALUES ('1', 'Zaino1', '[[\"marca\",\"codiice1\",\"codice2\"],[\"marca\",\"codiice1\",\"codice2\"]]');
+INSERT INTO `composition` VALUES ('2', 'Zaino2', '[[\"marca1\",\"codiice1\",\"codice2\"],[\"marca2\",\"codiice1\",\"codice2\"]]');
 
 -- ----------------------------
 -- Table structure for `products`
 -- ----------------------------
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
-  `ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `serial` varchar(255) DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `brand` varchar(255) DEFAULT NULL,
   `series` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
+INSERT INTO `products` VALUES ('1', '55556', '1.20', 'Uno', 'Uno', 'Uno');
+INSERT INTO `products` VALUES ('2', '55557', '3.20', 'Due', 'Uno', 'Uno');
 
 -- ----------------------------
 -- Table structure for `users`
@@ -63,9 +66,11 @@ CREATE TABLE `users` (
   `created_at` text DEFAULT NULL,
   `payed` int(11) DEFAULT 0,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('42', 'Nick', '$2b$05$NIifqYdBC7SkdyNAlI2S7OkRBMgC.uWgtbas1LfJp8Xf6jb7ie/P6', 'Nicholas', 'Fialdini', 'ddjnick03@gmail.com', '2020/01/10', '0');
+INSERT INTO `users` VALUES ('44', 'nick', '$2b$05$ecI.Y2h5MzrlRNKefxjkg.StzY3i1mD1EiidN1N7XaR7m3lj2xnH.', 'Nicholas', 'Fialdini', 'ddjnick03@gmail.com', '2020/1/8', '1');
+INSERT INTO `users` VALUES ('45', 'francesco', '$2b$05$NoErtubJ6ZvahKFLKfaZ7.bNx5CVozpBtcCR6lpRlXhQJtHFZXj2S', 'Francesco', 'Facchinetti', 'francesco@gmail.com', '2020/1/8', '1');
+INSERT INTO `users` VALUES ('46', 'Alex', '$2b$05$zpGCVqVGYBPkuWag8qbf7ecK0InHmtZSVWIAmsKyG5XgmW9ms8qte', 'Alex', 'Delsarto', 'alexajsajsa@gmail.com', '2020/1/25', '0');
